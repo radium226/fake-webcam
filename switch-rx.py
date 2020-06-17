02672e49040d20830fa48a6a234f8a6516cc3ac7
Adrien Besnard <adrien.besnard@ultra.io>
2020-06-17 10:46:00 +0200

    YYYAYYAYAYAYAAAA

--- switch-rx.py
+++ switch-rx.py
@@ -9,27 +9,65 @@ from time import sleep
 from queue import Queue
 from threading import Thread
 
+import itertools
+
 
 def from_queue(queue):
-    def dispose():
-        print("DiSpOsE")
-
-    def subscribe(observer: Observer, schedule = None, disposable = None):
-        print(disposable)
+    def on_subscribe(observer: Observer, schedule = None):
         def thread_target():
-            while True:
-                item = queue.get()
+            for item in iter(queue.get, None):
                 observer.on_next(item)
+        
         thread = Thread(target=thread_target)
+
+        def on_dispose():
+            queue.put(None)
+            thread.join()
+                
         thread.start()
+        return on_dispose
 
-    return rx.create(subscribe)
+    return rx.create(on_subscribe)
 
 
+def letters():
+     return rx.zip(rx.timer(1, 1), rx.from_iterable(range(ord('a'), ord('z') + 1))).pipe(ops.map(lambda tick_letter: chr(tick_letter[1])))
 
+
+def numbers():
+    return rx.zip(rx.timer(1, 1), rx.from_iterable(range(0, 100))).pipe(ops.map(lambda tick_number: tick_number[1]))
 
 
 if __name__ == "__main__":
+    queue = Queue()
+
+    connectable_numbers = numbers().pipe(ops.publish())
+    connectable_numbers.connect()
+
+    disposable = from_queue(queue).pipe(ops.switch_latest()).subscribe(lambda item: print(item))
+
+    #def queue_target_target():
+    print(" ==> letters()")
+    queue.put(letters())
+    sleep(5)
+    print(" ==> numbers()")
+    queue.put(connectable_numbers)
+    sleep(5)
+    print(" ==> letters()")
+    queue.put(letters())
+    sleep(5)
+    print(" ==> numbers()")
+    queue.put(connectable_numbers)
+    sleep(5)
+    print(" ==> disposable()")
+    disposable.dispose()
+    
+    #control_thread = Thread(target=control_thread_target)
+    #control_thread.start()
+
+    #control_thread.join()
+
+    '''
     queue = Queue()
 
     d = from_queue(queue).subscribe(lambda x: print(x))
@@ -46,6 +84,8 @@ if __name__ == "__main__":
 
     d.dispose()
 
+    sleep(1)
+'''
     '''
 
     numbers = rx.zip(rx.timer(1, 1), rx.from_iterable(range(1, 100))).pipe(
