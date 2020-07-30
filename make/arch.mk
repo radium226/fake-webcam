95ab68a2a91014b262e22a872e3f29dc1d320fbe
radium226 <adrien@adrien-ux31a.localdomain>
2020-07-30 18:56:29 +0200

    Embbed easing-functions directly into Arch package #FIXME

--- make/arch.mk
+++ make/arch.mk
@@ -2,7 +2,7 @@ packages/arch/fakewebcam-$(VERSION).tar.gz: dist/fakewebcam-$(VERSION).tar.gz
 	cp "dist/fakewebcam-$(VERSION).tar.gz" "packages/arch/fakewebcam-$(VERSION).tar.gz"
 
 
-packages/arch/fake-webcam-$(VERSION)-1-any.pkg.tar.xz: packages/arch/fakewebcam-$(VERSION).tar.gz
+packages/arch/fake-webcam-$(VERSION)-1-any.pkg.tar.zst: packages/arch/fakewebcam-$(VERSION).tar.gz
 	cd "packages/arch"
 	PACMAN=yay \
 		makepkg \
@@ -13,14 +13,14 @@ packages/arch/fake-webcam-$(VERSION)-1-any.pkg.tar.xz: packages/arch/fakewebcam-
 			--force
 
 .PHONY: arch-package
-arch-package: packages/arch/fake-webcam-$(VERSION)-1-any.pkg.tar.xz
+arch-package: packages/arch/fake-webcam-$(VERSION)-1-any.pkg.tar.zst
 
 
 .PHONY: arch-install
 arch-install: arch-package
-	yay -U 'packages/arch/fake-webcam-$(VERSION)-1-any.pkg.tar.xz' --noconfirm
+	yay -U 'packages/arch/fake-webcam-$(VERSION)-1-any.pkg.tar.zst' --noconfirm
 
 .PHONY: arch-clean
 arch-clean:
-	find "packages/arch" \( -name "*.tar.gz" -o -name "*.pkg.tar.xz" \) -print | xargs -I {} rm -f "{}"
+	find "packages/arch" \( -name "*.tar.gz" -o -name "*.pkg.tar.zst" \) -print | xargs -I {} rm -f "{}"
 	
